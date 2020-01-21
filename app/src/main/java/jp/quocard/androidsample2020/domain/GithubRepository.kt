35957207a9b60d7845f6b4f4ce2afa8f2f000433
Takeshi SAITO <t.saito@quo-digital.jp>
2020-01-21 19:59:48 +0900

    Interfaceを分離

--- app/src/main/java/jp/quocard/androidsample2020/domain/GithubRepository.kt
+++ app/src/main/java/jp/quocard/androidsample2020/domain/GithubRepository.kt
@@ -4,25 +4,14 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class GithubRepository {
-
-    //Retrofit
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(HTTPS_API_GITHUB_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private var githubService: GithubService = retrofit.create(GithubService::class.java)
-
+interface GithubRepository {
 
     //中断関数でAPIにリクエストし、レスポンスをsuspend functionで受け取る(一覧)
-    suspend fun getProjectList(userId: String): Response<List<Project>> =
-        githubService.getProjectList(userId)
+    suspend fun getProjectList(userId: String): Response<List<Project>>
 
 
     //中断関数でAPIにリクエストし、レスポンスをsuspend functionで受け取る(詳細)
-    suspend fun getProjectDetails(userID: String, projectName: String): Response<Project> =
-        githubService.getProjectDetails(userID, projectName)
+    suspend fun getProjectDetails(userID: String, projectName: String): Response<Project>
 
 
     //singletonでRepositoryインスタンスを返すFactory
@@ -32,7 +21,28 @@ class GithubRepository {
 
         val instance: GithubRepository
             @Synchronized get() {
-                return GithubRepository()
+                return GithubRepositoryImpl()
             }
     }
+}
+
+class GithubRepositoryImpl: GithubRepository {
+
+    //Retrofit
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(GithubRepository.HTTPS_API_GITHUB_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private var githubService: GithubService = retrofit.create(GithubService::class.java)
+
+
+    //中断関数でAPIにリクエストし、レスポンスをsuspend functionで受け取る(一覧)
+    override suspend fun getProjectList(userId: String): Response<List<Project>> =
+        githubService.getProjectList(userId)
+
+
+    //中断関数でAPIにリクエストし、レスポンスをsuspend functionで受け取る(詳細)
+    override suspend fun getProjectDetails(userID: String, projectName: String): Response<Project> =
+        githubService.getProjectDetails(userID, projectName)
 }
